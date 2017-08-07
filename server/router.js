88ecac65991c3f88525597668d8080c25bf6ee56
Juho Heikkilä <juho.heikkil@gmail.com>
2017-08-07 23:11:30 +0300

    remove index route, add api prefix

--- server/router.js
+++ server/router.js
@@ -4,9 +4,6 @@ const router = (app, express, knex) => {
     'use strict';
     const router = {};
 
-    app.get('/', (req, res) => {
-        res.render('index');
-    });
 };
 
 module.exports = router; 
