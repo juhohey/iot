14046a2139bc1cebfa771bf065c68bb26c72aeaa
Juho Heikkilä <juho.heikkil@gmail.com>
2017-08-14 22:47:31 +0300

    fix latest commit

--- server/api/reading.js
+++ server/api/reading.js
@@ -19,11 +19,11 @@ module.exports = (app, knex) =>{
     const schema = require('../models/reading');
     const model = schema(knex);
 
-    const getInstance = body =>{
+    const getInstance = body => {
         const device = body.device || 1;
         return {
             timestamp:toTimeStamp(body.timestamp), payload:body.payload, device
-        };
+        }
     }
 
     return {
@@ -32,6 +32,9 @@ module.exports = (app, knex) =>{
             put: async (req) => await model.create( R.merge( getInstance(req.body), {createdAt:toTimeStamp(Date.now()) } )),
             delete: async (req) => await model.delete(),
             
+            '/latest':{
+                get: async (req) => await model.getLatest(),
+            },
             '/:id':{
                 get: async (req) => await model.get(req.params.id),
                 post: async (req) => await model.update(parseInt(req.params.id), R.merge(getInstance(req.body), {updatedAt:toTimeStamp(Date.now())})),
