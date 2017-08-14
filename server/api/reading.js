2c86b176c225eb6c3b3de0c52b23024d3a666385
Juho Heikkilä <juho.heikkil@gmail.com>
2017-08-14 22:39:27 +0300

    add default device id

--- server/api/reading.js
+++ server/api/reading.js
@@ -19,9 +19,12 @@ module.exports = (app, knex) =>{
     const schema = require('../models/reading');
     const model = schema(knex);
 
-    const getInstance = body =>({
-        timestamp:toTimeStamp(body.timestamp), payload:body.payload
-    })
+    const getInstance = body =>{
+        const device = body.device || 1;
+        return {
+            timestamp:toTimeStamp(body.timestamp), payload:body.payload, device
+        };
+    }
 
     return {
         '/readings':{
@@ -29,9 +32,6 @@ module.exports = (app, knex) =>{
             put: async (req) => await model.create( R.merge( getInstance(req.body), {createdAt:toTimeStamp(Date.now()) } )),
             delete: async (req) => await model.delete(),
             
-            '/latest':{
-                get: async (req) => await model.getLatest(),
-            },
             '/:id':{
                 get: async (req) => await model.get(req.params.id),
                 post: async (req) => await model.update(parseInt(req.params.id), R.merge(getInstance(req.body), {updatedAt:toTimeStamp(Date.now())})),
